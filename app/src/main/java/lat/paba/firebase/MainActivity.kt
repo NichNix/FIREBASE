cdc49c3ebd1ceaf8f0d9e11aac1e5ef441eb9902
LAPTOP-B9KJSTFI\LENOVO <nicholasanthonyprasetyo@gmail.com>
2024-12-10 21:11:07 +0700

    initial

--- app/src/main/java/lat/paba/firebase/MainActivity.kt
+++ app/src/main/java/lat/paba/firebase/MainActivity.kt
@@ -1,12 +1,29 @@
 package lat.paba.firebase
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
+
+    var DataProvinsi = ArrayList<daftarProvinsi>()
+    lateinit var lvAdapter : SimpleAdapter
+    lateinit var _etProvinsi : EditText
+    lateinit var _etIbukota : EditText
+
+    var data: MutableList<Map<String, String>> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +33,74 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val db = Firebase.firestore
+
+        _etProvinsi = findViewById<EditText>(R.id.etProvinsi)
+        _etIbukota = findViewById<EditText>(R.id.etIbukota)
+
+        val _btSimpan = findViewById<Button>(R.id.btSimpan)
+        val _lvData = findViewById<ListView>(R.id.lvData)
+
+        lvAdapter = SimpleAdapter(
+            this,
+            data,
+            android.R.layout.simple_list_item_2,
+            arrayOf<String>("Pro","Ibu"),
+            intArrayOf(
+                android.R.id.text1,
+                android.R.id.text2
+            )
+        )
+        _lvData.adapter = lvAdapter
+
+        _btSimpan.setOnClickListener{
+            val Provinsi = _etProvinsi.text.toString()
+            val Ibukota = _etIbukota.text.toString()
+            TambahData(db,Provinsi,Ibukota)
+        }
+
     }
+
+    fun TambahData(db: FirebaseFirestore, Provinsi:String, Ibukota:String){
+        val dataBaru = daftarProvinsi(Provinsi, Ibukota)
+        db.collection("tbProvinsi")
+            .add(dataBaru)
+            .addOnSuccessListener {
+                _etProvinsi.setText("")
+                _etIbukota.setText("")
+                Log.d("Firebase", "Data Berhasil Disimpan")
+            }
+            .addOnFailureListener {
+                Log.d("Firebase", it.message.toString())
+            }
+    }
+
+    fun readData (db: FirebaseFirestore){
+        db.collection("tbProvinsi").get()
+            .addOnSuccessListener {
+                    result ->
+                DataProvinsi.clear()
+                for(document in result) {
+                    val readData = daftarProvinsi(
+                        document.data.get("provinsi").toString(),
+                        document.data.get("ibukota").toString()
+                    )
+                    DataProvinsi.add(readData)
+
+                    data.clear()
+                    DataProvinsi.forEach{
+                        val dt: MutableMap<String, String> = HashMap(2)
+                        dt["Pro"] = it.provinsi
+                        dt["Ibu"] = it.ibukota
+                        data.add(dt)
+                    }
+                }
+                lvAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener {
+                Log.d("Firebase", it.message.toString())
+            }
+    }
+
 }
